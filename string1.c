b87a3786d11c5731aba73e25cee240e408f9e333
Wabyoona <maxwell@gmail.com>
2022-11-16 13:26:30 -0800

    string1.c

--- string1.c
+++ string1.c
@@ -3,45 +3,23 @@ i#include "shell.h"
 
 
 /**
- *
- *  * _strcpy - copies a string
- *
- *   * @dest: the destination
- *
- *    * @src: the source
- *
- *     *
- *
- *      * Return: pointer to destination
- *
- *       */
-
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * Return: pointer to destination
+ */
 char *_strcpy(char *dest, char *src)
-
 {
-
-		int i = 0;
-
-
-
-			if (dest == src || src == 0)
-
-						return (dest);
-
-				while (src[i])
-
-						{
-
-									dest[i] = src[i];
-
-											i++;
-
-												}
-
-					dest[i] = 0;
-
-						return (dest);
-
+int i = 0;
+if (dest == src || src == 0)
+return (dest);
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = 0;
+return (dest);
 }
 /**
  * _strdup - duplicates a string
