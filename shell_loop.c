19f4cc6b326f47174afbb09a051548dfb8350b68
Wabyoona <maxwell@gmail.com>
2022-11-16 11:48:00 -0800

    shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -1,47 +1,24 @@
 #include "shell.h"
 
 
-
 /**
- *
- *  * hsh - main shell loop
- *
- *   * @info: the parameter & return info struct
- *
- *    * @av: the argument vector from main()
- *
- *     *
- *
- *      * Return: 0 on success, 1 on error, or error code
- *
- *       */
-
+ * hsh - main shell loop
+ * @info: the parameter & return info struct
+ * @av: the argument vector from main()
+ * Return: 0 on success, 1 on error, or error code
+ */
 int hsh(info_t *info, char **av)
-
 {
-
-		ssize_t r = 0;
-
-			int builtin_ret = 0;
-
-
-
-				while (r != -1 && builtin_ret != -2)
-
-						{
-
-									clear_info(info);
-
-											if (interactive(info))
-
-															_puts("$ ");
-
-													_eputchar(BUF_FLUSH);
-
-															r = get_input(info);
-
-																	if (r != -1)
+ssize_t r = 0;
+int builtin_ret = 0;
+while (r != -1 && builtin_ret != -2)
 {
+clear_info(info);
+if (interactive(info))
+_puts("$ ");
+_eputchar(BUF_FLUSH);
+r = get_input(info);
+if (r != -1)
 set_info(info, av);
 builtin_ret = find_builtin(info);
 if (builtin_ret == -1)
