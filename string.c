5efbe649faa7aff263ac1783452565453b857561
Wabyoona <maxwell@gmail.com>
2022-11-16 13:02:51 -0800

    string.c

--- string.c
+++ string.c
@@ -79,35 +79,18 @@ int _strcmp(char *s1, char *s2)
 							return (*s1 < *s2 ? -1 : 1);
 
 }
-
-
-
 /**
- *
- *  * starts_with - checks if needle starts with haystack
- *
- *   * @haystack: string to search
- *
- *    * @needle: the substring to find
- *
- *     *
- *
- *      * Return: address of next char of haystack or NULL
- *
- *       */
-
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ * Return: address of next char of haystack or NULL
+ */
 char *starts_with(const char *haystack, const char *needle)
-
 {
-
-		while (*needle)
-
-					if (*needle++ != *haystack++)
-
-									return (NULL);
-
-			return ((char *)haystack);
-
+while (*needle)
+if (*needle++ != *haystack++)
+return (NULL);
+return ((char *)haystack);
 }
 /**
  * _strcat - concatenates two strings
