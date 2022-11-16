ce455083714f0c6a7f1b159159841abf3e25b8f2
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-11-16 03:18:28 -0800

    added new file

--- shell.h
+++ shell.h
@@ -1,75 +1,62 @@
-#ifndef _SHELL_
-#define _SHELL_
+#ifndef SHELL_H
+#define SHELL_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
 
-#define BUFFER 1024
-#define TRUE 1
-#define PROMPT "$ "
-/* error strings */
-#define ERR_MALLOC "Unable to malloc space\n"
-#define ERR_FORK "Unable to fork and create child process\n"
-#define ERR_PATH "No such file or directory\n"
+/* environment variables */
 extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
-/**
- * struct list_s - linked list of variables
- * @value: value
- * @next: pointer to next node
- *
- * Description: generic linked list struct for variables.
-**/
-typedef struct list_s
-{
-	char *value;
-	struct list_s *next;
-} list_s;
+/* handle built ins */
+int checker(char **cmd, char *buf);
+void prompt_user(void);
+void handle_signal(int m);
+char **tokenizer(char *line);
+char *test_path(char **path, char *command);
+char *append_path(char *path, char *command);
+int handle_builtin(char **command, char *line);
+void exit_cmd(char **command, char *line);
 
-/**
- * struct built_s - linked list of builtins
- * @name: name of builtin
- * @p: pointer to function
- *
- * Description: struct for builtin functions.
-**/
-typedef struct built_s
-{
-	char *name;
-	int (*p)(void);
-} built_s;
+void print_env(void);
 
-void prompt(int fd, struct stat buf);
-char *_getline(FILE *fp);
-char **tokenizer(char *str);
-char *_which(char *command, char *fullpath, char *path);
-int child(char *fullpath, char **tokens);
-void errors(int error);
-
-/* utility functions */
-void _puts(char *str);
+/* string handlers */
+int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
-int _strcmp(char *name, char *variable, unsigned int length);
-int _strncmp(char *name, char *variable, unsigned int length);
-char *_strcpy(char *dest, char *src);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
 
-/* prototypes for builtins */
-int shell_env(void);
-int shell_exit(void);
-int builtin_execute(char **tokens);
-int shell_num_builtins(built_s builtin[]);
+void execution(char *cp, char **cmd);
+char *find_path(void);
 
-/* prototypes for the helper functions for path linked list */
-char *_getenv(const char *name);
-char **copy_env(char **environ_copy, unsigned int environ_length);
-list_s *pathlist(char *variable, list_s *head);
+/* helper function for efficient free */
+void free_buffers(char **buf);
 
-/* prototypes for free functions */
-void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
-void free_dp(char **array, unsigned int length);
-#endif
+struct builtin
+{
+	char *env;
+	char *exit;
+} builtin;
+
+struct info
+{
+	int final_exit;
+	int ln_count;
+} info;
+
+struct flags
+{
+	bool interactive;
+} flags;
+
+#endif /* SHELL_H */
+
