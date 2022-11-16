8f1cfcffd9203565a3800642a863f0c8fe26c7f8
Wabyoona <maxwell@gmail.com>
2022-11-16 13:00:12 -0800

    string.c

--- string.c
+++ string.c
@@ -109,41 +109,19 @@ char *starts_with(const char *haystack, const char *needle)
 			return ((char *)haystack);
 
 }
-
-
-
 /**
- *
- *  * _strcat - concatenates two strings
- *
- *   * @dest: the destination buffer
- *
- *    * @src: the source buffer
- *
- *     *
- *
- *      * Return: pointer to destination buffer
- *
- *       */
-
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ *  @src: the source buffer
+ * Return: pointer to destination buffer
+ */
 char *_strcat(char *dest, char *src)
-
 {
-
-		char *ret = dest;
-
-
-
-			while (*dest)
-
-						dest++;
-
-				while (*src)
-
-							*dest++ = *src++;
-
-					*dest = *src;
-
-						return (ret);
-
+char *ret = dest;
+while (*dest)
+dest++;
+while (*src)
+*dest++ = *src++;
+*dest = *src;
+return (ret);
 }
