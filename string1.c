5ff99ac506cdf959b039c2dc590baf69b6ff628a
Wabyoona <maxwell@gmail.com>
2022-11-16 13:22:29 -0800

    string1.c

--- string1.c
+++ string1.c
@@ -43,89 +43,41 @@ char *_strcpy(char *dest, char *src)
 						return (dest);
 
 }
-
-
-
 /**
- *
- *  * _strdup - duplicates a string
- *
- *   * @str: the string to duplicate
- *
- *    *
- *
- *     * Return: pointer to the duplicated string
- *
- *      */
-
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(const char *str)
-
 {
-
-		int length = 0;
-
-			char *ret;
-
-
-
-				if (str == NULL)
-
-							return (NULL);
-
-					while (*str++)
-
-								length++;
-
-						ret = malloc(sizeof(char) * (length + 1));
-
-							if (!ret)
-
-										return (NULL);
-
-								for (length++; length--;)
-
-											ret[length] = *--str;
-
-									return (ret);
-
+int length = 0;
+char *ret;
+if (str == NULL)
+return (NULL);
+while (*str++)
+length++;
+ret = malloc(sizeof(char) * (length + 1));
+if (!ret)
+return (NULL);
+for (length++; length--;)
+ret[length] = *--str;
+return (ret);
 }
-
-
-
 /**
- *
- *  *_puts - prints an input string
- *
- *   *@str: the string to be printed
- *
- *    *
- *
- *     * Return: Nothing
- *
- *      */
-
+ *_puts - prints an input string
+ *@str: the string to be printed
+ * Return: Nothing
+ */
 void _puts(char *str)
-
 {
-
-		int i = 0;
-
-
-
-			if (!str)
-
-						return;
-
-				while (str[i] != '\0')
-
-						{
-
-									_putchar(str[i]);
-
-											i++;
-
-												}
-
+int i = 0;
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 }
 /**
  * _putchar - writes the character c to stdout
