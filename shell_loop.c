72c47be8e2674a6d46da2e9287888675cf9519d4
Wabyoona <maxwell@gmail.com>
2022-11-16 11:37:14 -0800

    shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -41,161 +41,77 @@ int hsh(info_t *info, char **av)
 															r = get_input(info);
 
 																	if (r != -1)
-
-																				{
-
-																								set_info(info, av);
-
-																											builtin_ret = find_builtin(info);
-
-																														if (builtin_ret == -1)
-
-																																			find_cmd(info);
-
-																																}
-
-																			else if (interactive(info))
-
-																							_putchar('\n');
-
-																					free_info(info, 0);
-
-																						}
-
-					write_history(info);
-
-						free_info(info, 1);
-
-							if (!interactive(info) && info->status)
-
-										exit(info->status);
-
-								if (builtin_ret == -2)
-
-										{
-
-													if (info->err_num == -1)
-
-																	exit(info->status);
-
-															exit(info->err_num);
-
-																}
-
-									return (builtin_ret);
-
+{
+set_info(info, av);
+builtin_ret = find_builtin(info);
+if (builtin_ret == -1)
+find_cmd(info);
 }
-
-
-
+else if (interactive(info))
+_putchar('\n');
+free_info(info, 0);
+}
+write_history(info);
+free_info(info, 1);
+if (!interactive(info) && info->status)
+exit(info->status);
+if (builtin_ret == -2)
+{
+if (info->err_num == -1)
+exit(info->status);
+exit(info->err_num);
+}
+return (builtin_ret);
+}
 /**
- *
- *  * find_builtin - finds a builtin command
- *
- *   * @info: the parameter & return info struct
- *
- *    *
- *
- *     * Return: -1 if builtin not found,
- *
- *      *			0 if builtin executed successfully,
- *
- *       *			1 if builtin found but not successful,
- *
- *        *			-2 if builtin signals exit()
- *
- *         */
-
+ * find_builtin - finds a builtin command
+ * @info: the parameter & return info struct
+ * Return: -1 if builtin not found,
+ *			0 if builtin executed successfully,
+ *		1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
+ */
 int find_builtin(info_t *info)
-
 {
-
-		int i, built_in_ret = -1;
-
-			builtin_table builtintbl[] = {
-
-						{"exit", _myexit},
-
-								{"env", _myenv},
-
-										{"help", _myhelp},
-
-												{"history", _myhistory},
-
-														{"setenv", _mysetenv},
-
-																{"unsetenv", _myunsetenv},
-
-																		{"cd", _mycd},
-
-																				{"alias", _myalias},
-
-																						{NULL, NULL}
-
-							};
-
-
-
-				for (i = 0; builtintbl[i].type; i++)
-
-							if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
-
-										{
-
-														info->line_count++;
-
-																	built_in_ret = builtintbl[i].func(info);
-
-																				break;
-
-																						}
-
-					return (built_in_ret);
-
+int i, built_in_ret = -1;
+builtin_table builtintbl[] = {
+{"exit", _myexit},
+{"env", _myenv},
+{"help", _myhelp},
+{"history", _myhistory},
+{"setenv", _mysetenv},
+{"unsetenv", _myunsetenv},
+{"cd", _mycd},
+{"alias", _myalias},
+{NULL, NULL}
+};
+for (i = 0; builtintbl[i].type; i++)
+if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+{
+info->line_count++;
+built_in_ret = builtintbl[i].func(info);
+break;
 }
-
-
-
+return (built_in_ret);
+}
 /**
- *
- *  * find_cmd - finds a command in PATH
- *
- *   * @info: the parameter & return info struct
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
+ * find_cmd - finds a command in PATH
+ * @info: the parameter & return info struct
+ * Return: void
+ */
 void find_cmd(info_t *info)
-
 {
-
-		char *path = NULL;
-
-			int i, k;
-
-
-
-				info->path = info->argv[0];
-
-					if (info->linecount_flag == 1)
-
-							{
-
-										info->line_count++;
-
-												info->linecount_flag = 0;
-
-													}
-
-						for (i = 0, k = 0; info->arg[i]; i++)
-
-									if (!is_delim(info->arg[i], " \t\n"))
-
-													k++;
-
+char *path = NULL;
+int i, k;
+info->path = info->argv[0];
+if (info->linecount_flag == 1)
+{
+info->line_count++;
+info->linecount_flag = 0;
+}
+for (i = 0, k = 0; info->arg[i]; i++)
+if (!is_delim(info->arg[i], " \t\n"))
+k++;
 if (!k)
 return;
 path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
