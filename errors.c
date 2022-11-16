4627c28bf6dafd6f04cc4b40123f2e9ad6947be1
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-11-16 02:49:06 -0800

    added new file

--- errors.c
+++ errors.c
@@ -1,85 +1,31 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
+ * errors - prints errors based on case
+ * @error: error number associated with perror statement
+ * Return: void
  */
-void _eputs(char *str)
+void errors(int error)
 {
-	int i = 0;
+	switch (error)
+	{
+	case 1: /* writes fork error to stderr */
+		write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
+		perror("Error");
+		break;
 
-	if (!str)
+	case 2: /* writes execve error to stderr */
+		perror("Error");
+		break;
+
+	case 3: /* writes malloc error to stderr */
+		write(STDERR_FILENO, ERR_MALLOC, _strlen(ERR_MALLOC));
+		break;
+	case 4: /* writes empty path error to stderr */
+		write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
+		break;
+
+	default:
 		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
 	}
-}
-
-/**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _eputchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
-
-/**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putfd(char c, int fd)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
-
-/**
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
-int _putsfd(char *str, int fd)
-{
-	int i = 0;
-
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
 }
