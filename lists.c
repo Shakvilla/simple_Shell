20ec7988dd9f0619bc38f92c343cd351b2cfc879
Wabyoona <maxwell@gmail.com>
2022-11-16 14:03:36 -0800

    lists.c

--- lists.c
+++ lists.c
@@ -1,22 +1,12 @@
 #include "shell.h"
 
-
-
 /**
- *
- *  * add_node - adds a node to the start of the list
- *
- *   * @head: address of pointer to head node
- *
- *    * @str: str field of node
- *
- *     * @num: node index used by history
- *
- *      *
- *
- *       * Return: size of list
- *
- *        */
+ * add_node - adds a node to the start of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
+ * Return: size of list
+ */
 list_t *add_node(list_t **head, const char *str, int num)
 {
 list_t *new_head;
